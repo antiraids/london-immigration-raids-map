393ab95e8d2724aada9fa36257062927f7d08802
Sami Capulcu <setatimmi@gmail.com>
2023-05-28 18:45:54 +0100

    Plots with correct styling

--- deportation_data_investigation.py
+++ deportation_data_investigation.py
@@ -9,9 +9,25 @@ import pandas as pd
 import matplotlib.pyplot as plt; plt.style.use('ggplot')
 import seaborn as sns
 
-# https://www.gov.uk/government/statistical-data-sets/returns-and-detention-datasets#immigration-detention
-
 fp = 'RawData\\returns-datasets-dec-2022.xlsx'
+
+# =============================================================================
+# Get plt parameters to match other graphs, from TellingTheStory
+# =============================================================================
+
+plt.rcParams['figure.figsize'] = (15, 11)
+plt.rcParams['axes.titlesize'] = 28
+plt.rcParams['axes.labelsize'] = 16
+plt.rcParams['axes.edgecolor'] = '#A9A9A9'
+plt.rcParams['axes.spines.top'] = False
+plt.rcParams['axes.spines.right'] = False
+plt.rcParams['savefig.transparent'] = True
+
+watermark_str = "Enforced return data from Gov.uk, covering 2016-2022. More at https://antiraids.github.io"
+
+# =============================================================================
+# Get data
+# =============================================================================
 
 # Get the right sheet name
 xls = pd.ExcelFile(fp)
@@ -22,6 +38,10 @@ df = pd.read_excel(fp, sheet_name='Data - Ret_D02', header=1)
 df.head(1).T
 df_slc = df[df['Return type group'] == 'Enforced return']
 df_slc = df_slc[df_slc['Year'] >= 2022]
+
+# =============================================================================
+# 2022 data: investigation
+# =============================================================================
 
 by_country = df_slc.groupby('Return destination').sum()
 by_region = df_slc.groupby('Return destination region').sum()
@@ -48,7 +68,10 @@ ax.annotate('Only includes destinations with >=20 deportations to them. Data\
 plt.tight_layout()
 plt.savefig('Outputs\\Deportations_by_place_2022.png')
 
-# Since 2016
+# =============================================================================
+# Since 2016, totals
+# =============================================================================
+
 df_slc = df[df['Return type group'] == 'Enforced return']
 df_slc = df_slc[df_slc['Year'] >= 2016]
 by_place = df_slc.groupby(['Return destination', 'Return destination region']).sum()
@@ -67,11 +90,13 @@ colorblind_palette = {
     'Other': '#000000',  # black
     'Africa Sub-Saharan': '#0072B2'  # blue
         }  # because this is the same order as the legend, this work below too
-fig, ax = plt.subplots(1, 1, figsize=(8, 5))
+fig, ax = plt.subplots()
 sns.barplot(data=by_place, x='Deportations', y='Destination',
             hue='Area', dodge=False, ax=ax, palette=colorblind_palette.values())
-ax.set_title('Top 20 countries deported to since 2016', fontsize=25)
+ax.set_title('Top 20 countries deported to since 2016')
 ax.set_ylabel('')
+plt.annotate(watermark_str, (0.03, 0.03), color='grey', fontsize=15,
+             xycoords='figure fraction')
 plt.tight_layout()
 plt.savefig('Outputs\\Deportations_by_place_2016.png')
 
@@ -79,7 +104,10 @@ plt.savefig('Outputs\\Deportations_by_place_2016.png')
 for i in by_place.index:
     print(f'{by_place.loc[i, "Destination"]}: {by_place.loc[i, "Deportations"]: .0f}')
 
-# Over time
+# =============================================================================
+# Since 2016, by year
+# =============================================================================
+
 by_time_20 = df_slc.groupby(['Return destination', 'Return destination region', 'Year']).sum()
 by_time_20 = by_time_20.reset_index().sort_values('Number of returns')
 by_time_20 = by_time_20[by_time_20['Return destination'].isin(by_place['Destination'])]
